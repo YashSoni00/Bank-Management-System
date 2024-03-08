656be599ef97d8cd392e5f3f7261e00325cc3b54
YashSoni00 <ysyashsoni27@gmail.com>
2024-03-08 13:41:14 +0530

    Added Main

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+// This is the main class of the project
+// The main method is the entry point of the program
+
+// Main -> Controller -> Service -> Data Access -> Domain
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
